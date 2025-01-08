a897b492c95bc4aa712acff72b2aef9913bf8334
Anita Palhares Bringhenti <182400147@senacinformatica.br>
2025-01-08 17:16:34 -0300

    tk1

--- PrincipalGame.py
+++ PrincipalGame.py
@@ -1,3 +1,25 @@
-import random
+import tkinter as tk
+
+root = tk.Tk()  # Cria a janela principal
+root.title("Minha Janela com Grid") # Define o título da janela
+root.geometry("500x500")    # Define o tamanho da janela (opcional)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Inicia o loop da interface gráfica
+root.mainloop()
